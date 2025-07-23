1b937bd5711f5ba5ff8924d029c950515e256158
MoreeZ <github@example.com>
2025-07-23 14:34:59 +0100

    Updated ports

--- server/main.py
+++ server/main.py
@@ -105,4 +105,4 @@ async def trigger_five_seconds():
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server:app", host="127.0.0.1", port=420, reload=True)
